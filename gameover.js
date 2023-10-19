6a0d611b43bccbb32ab5445b0bf4fe7b282e4e98
Jaiabhisshek8 <jaiabhissheks@gmail.com>
2023-10-19 23:36:35 +0530

    COMPLETED

--- gameover.js
+++ gameover.js
@@ -1,25 +1,25 @@
 
-// let playAgainButton = document.getElementById("play-again-button")
-// let finalScore = document.getElementById("final-score")
-// let endTxt = document.getElementById("end-txt")
-// let textarray = ["WORK HARD", "GOOD ONE", "GREAT JOB SOLDIER"];
+let playAgainButton = document.getElementById("play-again-button")
+let finalScore = document.getElementById("final-score")
+let endTxt = document.getElementById("end-txt")
+let textarray = ["WORK HARD", "GOOD ONE", "GREAT JOB SOLDIER"];
 
-// let score = localStorage.getItem("score");
+let score = localStorage.getItem("score");
 
-// function checkscore(){
-//     if(score<=50){
-//         endTxt.textContent = textarray[0];
-//     }else if(50<score<=100){
-//         endTxt.textContent = textarray[1];
-//     }else{
-//         endTxt.textContent = textarray[2];
-//     }
-// }
+function checkscore(){
+    if(score<=50){
+        endTxt.textContent = textarray[0];
+    }else if(50<score<=100){
+        endTxt.textContent = textarray[1];
+    }else{
+        endTxt.textContent = textarray[2];
+    }
+}
 
-// checkscore();
+checkscore();
 
-// playAgainButton.onclick = function(){
-//     location.href = "index.html"
-// }
+playAgainButton.onclick = function(){
+    location.href = "index.html"
+}
 
-// finalScore.textContent = localStorage.getItem("score")
+finalScore.textContent = localStorage.getItem("score")
